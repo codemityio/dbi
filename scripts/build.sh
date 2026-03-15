d66fb1a41f659d4ba73ae6008372b63951b0dfd7
Krzysztof Zaucha <krzysztof@zaucha.co.uk>
2026-03-15 23:05:30 +0000

    configure build pipeline to list img alphabetically

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Note: the set [-/+] x is purely there to turn on and off outputting of the commands being executed.
 if [ "${DEBUG}" = "true" ]; then
   set -x
 fi
 
-images=$(find img/*/ -maxdepth 0 -type d -exec basename {} \;)
+images=$(find img/*/ -maxdepth 0 -type d -exec basename {} \; | sort)
 
 for target in ${images//,/ }; do
   docker image build \
